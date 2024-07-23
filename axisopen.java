14e5b19ffac7625a04105dbca90ab74f45261aab
Paramesh <parames.m@gmail.com>
2024-07-23 06:14:08 +0000

    print message updated

--- axisopen.java
+++ axisopen.java
@@ -3,7 +3,7 @@
 public class CountCharacter
 {
     public static void main(String[] args) {
-        String string = "The best of both worlds";
+        String string = "The best of both worlds : ";
         int count = 0;
         for(int i = 0; i < string.length(); i++) {
             if(string.charAt(i) != ' ')
